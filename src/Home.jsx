ac8ff1454e0cb63a6de853a7810b605fb580e678
hamza <mmalikhamza44@gmail.com>
2020-11-11 18:49:55 +0500

    my first commit reactwebsite

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import web from '../src/images/img1.jpg'
+import {NavLink} from 'react-router-dom'
+import Common from './Common'
+
+ const Home = () =>{
+  return (
+    <div>
+   <Common 
+       name='Grow your business with'
+       imgsrc={web}
+       visit ='/service'
+       btname='Get Started'
+
+       />
+    </div>
+  );
+}
+
+export default Home;
